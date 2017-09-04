b7919fe50a46825d72e71cbe89c410763d1104f5
Yusuf Pisan <pisan@uw.edu>
2017-09-03 20:13:33 -0700

    initial

---
+++ assignments/ass01/brokenNumberswithPointers.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+/* Write 3 functons
+ * sum2, diff2 and product2
+ * to make the following program work.
+ *
+ * You are not allowed to modify compareNumbers 
+ *
+ */
+
+void compareNumbers ()
+{
+    int * ff;
+    ff = &firstNum;
+    int firstNum = 5;
+    int secondNum = 2;
+    int dif;
+    int sum = sum2(ff, secondNum);
+    diff2(ff, &secondNum, &dif);
+    int prod = product2(*ff, &secondNum);
+    assert(sum == 7);
+    assert(dif == 3);
+    assert(prod == 10);
+}
+
+
+int main()
+{
+    compareNumbers();
+    printf("If we got here, all is well!\n");
+    return 0;
+}
