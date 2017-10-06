23dea49536d92a3baf06e5be5919dcafdcf3f9ee
unknown <pisan@UW2-240-CPC.uwb.edu>
2017-10-05 19:03:15 -0700

    initial

---
+++ classexercises/week02/MessyAssignment/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x = 10;
+    if (x = 20)
+    {
+        printf("The expression x=20 evaluated to true, x is : %d\n", x);
+    }
+    if (x = 0)
+    {
+        printf("The expression x=0 evaluated to true, x is : %d\n", x);
+    }
+
+    return 0;
+}
